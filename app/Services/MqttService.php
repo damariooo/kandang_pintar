773559b01aa287cb6dcb501f6a890308dde72239
hyodouuuuu <lordharem90@gmail.com>
2026-05-19 12:54:56 +0700

    mqtt part2

--- app/Services/MqttService.php
+++ app/Services/MqttService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use PhpMqtt\Client\MqttClient;
+use PhpMqtt\Client\ConnectionSettings;
 
 class MqttService
 {
@@ -15,7 +16,16 @@ class MqttService
 
         $mqtt = new MqttClient($server, $port, $clientId);
 
-        $mqtt->connect();
+        // 2. TAMBAHKAN CONFIG ENKRIPSI DAN LOGIN HIVEMQ DI SINI
+        $settings = (new ConnectionSettings)
+            ->setUseTls(true)                  // Mengaktifkan TLS (karena pakai port 8883)
+            ->setTlsSelfSignedAllowed(true)    // Mengizinkan self-signed certificate
+            ->setTlsVerifyPeer(false)          // Mem-bypass verifikasi ketat (mirip setInsecure di ESP32)
+            ->setUsername('ayokawan')          // Username HiveMQ kamu
+            ->setPassword('Ayokawan123');      // Password HiveMQ kamu
+
+        // 3. MASUKKAN SETTINGAN KE DALAM METHOD CONNECT
+        $mqtt->connect($settings);
 
         $mqtt->publish($topic, $message);
 
